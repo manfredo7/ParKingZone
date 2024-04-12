c5e6d9c81ffec9e3aefdfd239e2c9305a75a7198
OscaRMata1263 <119538095+OscaRMata1263@users.noreply.github.com>
2024-04-12 16:20:06 -0500

    Add files via upload

---
+++ ParkingZone/src/main/java/pe/edu/upc/parkingzone/dtos/TpagoDTO.java
@@ -0,0 +1,17 @@
+package pe.edu.upc.parkingzone.dtos;
+//Desarrollo Ricardo
+public class TpagoDTO {
+    private int TipoPagoID;
+    private String descripcionTpago;
+
+    public int getTipoPagoID() {return TipoPagoID;}
+
+    public void setTipoPagoID(int TipoPagoID) {
+        this.TipoPagoID = TipoPagoID;
+    }
+
+    public String getDescripcionTpago() {return descripcionTpago;}
+
+    public void setDescripcionTpago (String descripcionTpago) { this.descripcionTpago = descripcionTpago;}
+
+}
