02b589031c97a598a72c31fdfc76edd37990fa21
Luciano <82598695+manfredo7@users.noreply.github.com>
2024-04-12 12:01:45 -0500

    Delete ParKingZone directory

--- ParKingZone/src/main/java/pe/edu/upc/ParKingZone/repositories/IMembresiaRepository.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.upc.ParKingZone.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import pe.edu.upc.ParKingZone.entities.Membresia;
-
-@Repository
-public interface IMembresiaRepository extends JpaRepository<Membresia, Integer> {
-}
--- ParKingZone/src/main/java/pe/edu/upc/ParKingZone/repositories/IUsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.upc.ParKingZone.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import pe.edu.upc.ParKingZone.entities.Usuario;
-
-@Repository
-public interface IUsuarioRepository extends JpaRepository<Usuario, Integer> {
-}
