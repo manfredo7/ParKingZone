a0f9db59ed995a725b7dcc35e89364a08815698b
Luciano <82598695+manfredo7@users.noreply.github.com>
2024-04-23 01:26:11 -0500

    Add files via upload

---
+++ ParkingZone/src/main/java/pe/edu/upc/parkingzone/ParkingZoneApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.parkingzone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingZoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingZoneApplication.class, args);
+	}
+
+}
---
+++ ParkingZone/src/main/java/pe/edu/upc/parkingzone/controllers/MembresiaController.java
@@ -0,0 +1,40 @@
+package pe.edu.upc.parkingzone.controllers;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import pe.edu.upc.parkingzone.dtos.MembresiaDTO;
+import pe.edu.upc.parkingzone.entities.Membresia;
+
+import pe.edu.upc.parkingzone.serviceinterfaces.IMembresiaService;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/membresia")
+public class MembresiaController {
+    @Autowired
+    private IMembresiaService mS;
+
+    @PostMapping
+    public void insertar(@RequestBody MembresiaDTO membresiaDTO){
+        ModelMapper m=new ModelMapper();
+        Membresia mm =m.map(membresiaDTO,Membresia.class);
+        mS.insert(mm);
+    }
+
+    @GetMapping
+    public List<MembresiaDTO> listar(){
+        return mS.list().stream().map(y->{
+            ModelMapper m=new ModelMapper();
+            return m.map(y,MembresiaDTO.class);
+        }).collect(Collectors.toList());
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable("id") Integer id){
+        mS.delete(id);
+    }
+}
