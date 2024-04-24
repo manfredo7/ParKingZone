411ab621309661f5817531d24181695b6f45b366
Luciano <u202016625@upc.edu.pe>
2024-04-24 03:27:22 -0500

    version con querys Luciano

---
+++ src/main/java/pe/edu/upc/parkingzone/controllers/MembresiaController.java
@@ -0,0 +1,50 @@
+package pe.edu.upc.parkingzone.controllers;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import pe.edu.upc.parkingzone.dtos.MembresiaDTO;
+import pe.edu.upc.parkingzone.entities.Membresia;
+
+import pe.edu.upc.parkingzone.serviceinterfaces.IMembresiaService;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/membresia")
+public class MembresiaController {
+    @Autowired
+    private IMembresiaService mS;
+
+    //Reg
+    @PostMapping
+    public void insertar(@RequestBody MembresiaDTO membresiaDTO){
+        ModelMapper m=new ModelMapper();
+        Membresia mm =m.map(membresiaDTO,Membresia.class);
+        mS.insert(mm);
+    }
+    //Vis
+    @GetMapping
+    public List<MembresiaDTO> listar(){
+        return mS.list().stream().map(y->{
+            ModelMapper m=new ModelMapper();
+            return m.map(y,MembresiaDTO.class);
+        }).collect(Collectors.toList());
+    }
+    //Eliminar
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable("id") Integer id){
+        mS.delete(id);
+    }
+    //buscar por id
+    @GetMapping("/{id}")
+    public MembresiaDTO listarId(@PathVariable("id") Integer id){
+        ModelMapper m=new ModelMapper();
+        MembresiaDTO dto=m.map(mS.listId(id),MembresiaDTO.class);
+        return dto;
+    }
+
+}
---
+++ src/main/java/pe/edu/upc/parkingzone/controllers/TestacionamientoController.java
@@ -0,0 +1,53 @@
+package pe.edu.upc.parkingzone.controllers;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import pe.edu.upc.parkingzone.dtos.MembresiaDTO;
+import pe.edu.upc.parkingzone.dtos.TestacionamientoDTO;
+import pe.edu.upc.parkingzone.entities.Testacionamiento;
+
+import pe.edu.upc.parkingzone.serviceinterfaces.ITestacionamientoService;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+@RestController
+@RequestMapping("/testacionamiento")
+public class TestacionamientoController {
+    @Autowired
+    private ITestacionamientoService teS;
+    //r
+    @PostMapping
+    public void insertar(@RequestBody TestacionamientoDTO testacionamientoDTO){
+        ModelMapper m=new ModelMapper();
+        Testacionamiento te =m.map(testacionamientoDTO,Testacionamiento.class);
+        teS.insert(te);
+    }
+    //v
+    @GetMapping
+    public List<TestacionamientoDTO> listar(){
+        return teS.list().stream().map(y->{
+            ModelMapper m=new ModelMapper();
+            return m.map(y,TestacionamientoDTO.class);
+        }).collect(Collectors.toList());
+    }
+    //eli
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable("id") Integer id){
+        teS.delete(id);
+    }
+
+    //buscar por fechaInicio
+    @GetMapping("/buscarDescripcion")
+    public List<TestacionamientoDTO>buscarDescripcion(@RequestParam String descripcion){
+        return teS.findByDescripcionTestacionamiento(descripcion).stream().map(y->{
+            ModelMapper m=new ModelMapper();
+            return m.map(y, TestacionamientoDTO.class);
+        }).collect(Collectors.toList());
+    }
+
+
+}
