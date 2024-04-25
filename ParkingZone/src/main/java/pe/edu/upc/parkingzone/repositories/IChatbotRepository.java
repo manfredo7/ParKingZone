d6b7d7616a9da25cfc259310e8b9653ee25c3174
Luciano <82598695+manfredo7@users.noreply.github.com>
2024-04-25 00:07:51 -0500

    Delete ParkingZone directory

--- ParkingZone/src/main/java/pe/edu/upc/parkingzone/repositories/IChatbotRepository.java
+++
@@ -1,7 +0,0 @@
-package pe.edu.upc.parkingzone.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pe.edu.upc.parkingzone.entities.Chatbot;
-
-public interface IChatbotRepository extends JpaRepository<Chatbot, Integer> {
-}
